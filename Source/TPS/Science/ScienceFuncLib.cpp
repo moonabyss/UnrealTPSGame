9c6d9c8782bbce2e57cbd9342fd6385f7aee09cd
moonabyss <d-v@ukr.net>
2023-05-09 18:15:19 +0300

    info doxygen

--- Source/TPS/Science/ScienceFuncLib.cpp
+++ Source/TPS/Science/ScienceFuncLib.cpp
@@ -4,7 +4,7 @@
 
 DEFINE_LOG_CATEGORY_STATIC(LogScience, All, All);
 
-int32 UScienceFuncLib::Fibonacci(int32 Value)
+int32 UScienceFuncLib::Fibonacci(const int32 Value)
 {
     // check(Value >= 0);
     if (Value < 0)
@@ -12,4 +12,17 @@ int32 UScienceFuncLib::Fibonacci(int32 Value)
         UE_LOG(LogScience, Error, TEXT("Invalid input for Fibonacci: %i"), Value);
     }
     return Value <= 1 ? Value : Fibonacci(Value - 1) + Fibonacci(Value - 2);
+}
+
+int32 UScienceFuncLib::Factorial(const int32 Value)
+{
+    if (Value < 0) return -1;
+
+    int32 Fac = 1;
+    for (int i = 1; i < Value; ++i)
+    {
+        Fac *= i;
+    }
+
+    return Fac;
 }
--- Source/TPS/Science/ScienceFuncLib.h
+++ Source/TPS/Science/ScienceFuncLib.h
@@ -13,5 +13,14 @@ class TPS_API UScienceFuncLib : public UBlueprintFunctionLibrary
 
 public:
     UFUNCTION(Category = "Science", BlueprintPure)
-    static int32 Fibonacci(int32 Value);
+    static int32 Fibonacci(const int32 Value);
+
+    /**
+     * @brief Calculates factorial for the input number.
+     * https://en.wikipedia.org/wiki/Factorial
+     * @param   [in] Value  Number for which factorial should be calculated.
+     * @return              Factorial or (-1) for negative numbers
+     */
+    UFUNCTION(Category = "Science", BlueprintPure)
+    static int32 Factorial(const int32 Value);
 };
