e6724ea14ae5163d9ede6d8f3b3d38516e140c6b
moonabyss <d-v@ukr.net>
2023-05-14 13:32:28 +0300

    update

--- devops/tests/copy_editor_settings.bat
+++ devops/tests/copy_editor_settings.bat
@@ -3,5 +3,5 @@
 call "%~dp0\..\config.bat"
 
 rem copy minimal editor settings
-mkdir "%~dp0Saved\Config\WindowsEditor"
+mkdir "Saved\Config\WindowsEditor"
 copy /Y "devops\tests\templates\EditorPerProjectUserSettings.ini" "Saved\Config\WindowsEditor\EditorPerProjectUserSettings.ini"
