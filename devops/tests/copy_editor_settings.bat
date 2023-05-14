78b6a2adf78302841241cb56b78db39062652f26
moonabyss <d-v@ukr.net>
2023-05-14 13:09:38 +0300

    update pathes in script

--- devops/tests/copy_editor_settings.bat
+++ devops/tests/copy_editor_settings.bat
@@ -4,4 +4,4 @@ call "%~dp0\..\config.bat"
 
 rem copy minimal editor settings
 mkdir "%~dp0Saved\Config\WindowsEditor"
-copy /Y "%~dp0devops\tests\templates\EditorPerProjectUserSettings.ini" "%~dp0Saved\Config\WindowsEditor\EditorPerProjectUserSettings.ini"
+copy /Y "devops\tests\templates\EditorPerProjectUserSettings.ini" "Saved\Config\WindowsEditor\EditorPerProjectUserSettings.ini"
